b77a358cace0c0721975161ae922377728a56801
astu <astu@kapsi.fi>
2018-10-05 18:48:24 +0300

    set token in blogs-service

--- bloglist-frontend-master/src/services/blogs.js
+++ bloglist-frontend-master/src/services/blogs.js
@@ -1,9 +1,25 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+const config = {
+  headers: {
+    'content-type': 'application/json',
+  }
+}
+
+const setToken = (token) => {
+  config.headers['authorization'] = 'bearer ' + token
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll}
+const post = (values) => {
+  return axios.post(baseUrl, JSON.stringify(values), config)
+    .then( (a) => a )
+    .catch( (a) => a );
+}
+
+export default { setToken, getAll, post }
